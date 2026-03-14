dee0a3638cb01df4f4dc1a0f36dab6be8f0b2ed9
raziel7893 <raziel7893@gmail.com>
2026-03-14 18:01:58 +0100

    cleanup variables that are already presend from inheriting from SteamAgent

--- Valheim.cs/Valheim.cs
+++ Valheim.cs/Valheim.cs
@@ -40,9 +40,7 @@ namespace WindowsGSM.Plugins
         public override string AppId => "896660"; // Game server appId
 
         // - Standard Constructor and properties
-        public Valheim(ServerConfig serverData) : base(serverData) => base.serverData = _serverData = serverData;
-        private readonly ServerConfig _serverData;
-        public string Error, Notice;
+        public Valheim(ServerConfig serverData) : base(serverData) => base.serverData = serverData;
 
 
         // - Game server Fixed variables
@@ -58,7 +56,7 @@ namespace WindowsGSM.Plugins
         public string QueryPort = "2457"; // Default query port
         public string Defaultmap = "Dedicated"; // Default map name
         public string Maxplayers = "10"; // Default maxplayers
-        public string Additional = "-password \"CHANGE_ME\" -savedir \"valheim\" -Public 1"; // Additional server start parameter
+        public string Additional = "-password \"CHANGE_ME\" -savedir \"c:\valheim\" -Public 1"; // Additional server start parameter
 
 
         // - Create a default cfg for the game server after installation
@@ -71,7 +69,7 @@ namespace WindowsGSM.Plugins
         public async Task<Process> Start()
         {
 
-            string shipExePath = Functions.ServerPath.GetServersServerFiles(_serverData.ServerID, StartPath);
+            string shipExePath = Functions.ServerPath.GetServersServerFiles(serverData.ServerID, StartPath);
             if (!File.Exists(shipExePath))
             {
                 Error = $"{Path.GetFileName(shipExePath)} not found ({shipExePath})";
@@ -81,17 +79,17 @@ namespace WindowsGSM.Plugins
             // Prepare start parameter
             var param = new StringBuilder();
             param.Append("-batchmode -nographics -crossplay");
-            param.Append(string.IsNullOrWhiteSpace(_serverData.ServerPort) ? string.Empty : $" -port {_serverData.ServerPort}");
-            param.Append(string.IsNullOrWhiteSpace(_serverData.ServerName) ? string.Empty : $" -name \"{_serverData.ServerName}\"");
-            param.Append(string.IsNullOrWhiteSpace(_serverData.ServerMap) ? string.Empty : $" -world \"{_serverData.ServerMap}\"");
-            param.Append(string.IsNullOrWhiteSpace(_serverData.ServerParam) ? string.Empty : $" {_serverData.ServerParam}");
+            param.Append(string.IsNullOrWhiteSpace(serverData.ServerPort) ? string.Empty : $" -port {serverData.ServerPort}");
+            param.Append(string.IsNullOrWhiteSpace(serverData.ServerName) ? string.Empty : $" -name \"{serverData.ServerName}\"");
+            param.Append(string.IsNullOrWhiteSpace(serverData.ServerMap) ? string.Empty : $" -world \"{serverData.ServerMap}\"");
+            param.Append(string.IsNullOrWhiteSpace(serverData.ServerParam) ? string.Empty : $" {serverData.ServerParam}");
 
             // Prepare Process
             var p = new Process
             {
                 StartInfo =
                 {
-                    WorkingDirectory = ServerPath.GetServersServerFiles(_serverData.ServerID),
+                    WorkingDirectory = ServerPath.GetServersServerFiles(serverData.ServerID),
                     FileName = shipExePath,
                     Arguments = param.ToString(),
                     WindowStyle = ProcessWindowStyle.Minimized,
@@ -107,7 +105,7 @@ namespace WindowsGSM.Plugins
                 p.StartInfo.RedirectStandardInput = true;
                 p.StartInfo.RedirectStandardOutput = true;
                 p.StartInfo.RedirectStandardError = true;
-                var serverConsole = new ServerConsole(_serverData.ServerID);
+                var serverConsole = new ServerConsole(serverData.ServerID);
                 p.OutputDataReceived += serverConsole.AddOutput;
                 p.ErrorDataReceived += serverConsole.AddOutput;
 
